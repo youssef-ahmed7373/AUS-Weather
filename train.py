ced69fd2552e6869a55b0dacb319392b4f635eab
Youssef Ahmed <youssef.ahmed773377@gmail.com>
2026-04-15 01:23:07 +0200

    update train to make prediction file

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from metrics_and_plots import plot_confusion_matrix, save_metrics
+from metrics_and_plots import plot_confusion_matrix, save_metrics,save_predictions,save_roc_curve
 from model import evaluate_model, train_model
 from utils_and_constants import PROCESSED_DATASET, TARGET_COLUMN
 
@@ -28,7 +28,8 @@ def main():
 
     save_metrics(metrics)
     plot_confusion_matrix(model, X_test, y_test)
-
+    save_predictions(y_test, y_pred)
+    save_roc_curve(y_test, y_pred_proba)
 
 if __name__ == "__main__":
     main()
